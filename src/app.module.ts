407820e8cff0d630fa24e7bbb8d92008def1b0ae
Julio Brazao <julio@brazao.dev>
2025-03-24 21:57:50 -0400

    Import new DB modules to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MysqlModule } from './infra/providers/mysql.module';
+import { MongoModule } from './infra/providers/mongo.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), MysqlModule, MongoModule],
   controllers: [AppController],
   providers: [AppService],
 })
