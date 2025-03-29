ff07028d964b57c21a019bfbdc26a0fb5693239f
Julio Brazao <julio@brazao.dev>
2025-03-29 01:34:10 -0400

    Strategy for KC connection

---
+++ src/infra/auth/keycloak.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-keycloak';
+
+@Injectable()
+export class KeycloakStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    const clientID = configService.get<string>('KEYCLOAK_CLIENT_ID');
+    const clientSecret = configService.get<string>('KEYCLOAK_CLIENT_SECRET');
+    const realm = configService.get<string>('KEYCLOAK_REALM');
+    const serverUrl = configService.get<string>('KEYCLOAK_SERVER_URL');
+    const redirectUri = configService.get<string>('KEYCLOAK_REDIRECT_URI');
+
+    super({
+      clientID,
+      clientSecret,
+      realm,
+      serverUrl,
+      redirectUri,
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, username: payload.preferred_username };
+  }
+}
