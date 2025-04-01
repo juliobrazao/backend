ceaa798b2519eb4f00ede2116a9c566bb16a126a
Julio Brazao <julio@brazao.dev>
2025-03-31 20:27:30 -0400

    Cleaning controller to interact with service

--- src/presentation/controllers/user.controller.ts
+++ src/presentation/controllers/user.controller.ts
@@ -1,39 +1,13 @@
 import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
-import { UserResponseDTO } from '@/presentation/dtos/users/user.response.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { CreateUserRequestDTO } from '@/presentation/dtos/users/create-user.request.dto';
 import { CreateUserUseCase } from '@/domain/usecases/create-user.usecase';
+import { CreateUserRequestDTO } from '../dtos/users/create-user.request.dto';
+import { GetUserResponseDTO } from '../dtos/users/get-user.response.dto';
 
 @ApiTags('Users')
 @Controller('user')
 export class UserController {
   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
-
-  @Get('all')
-  @ApiOperation({
-    summary: 'List all users',
-  })
-  @ApiResponse({
-    status: 200,
-    description: 'Users list fetched successfully',
-    type: [UserResponseDTO],
-  })
-  async getUsers(): Promise<Partial<UserResponseDTO[]>> {
-    return [{}] as Partial<UserResponseDTO[]>;
-  }
-
-  @Get(':id')
-  @ApiOperation({
-    summary: 'Get a specific user by ID',
-  })
-  @ApiResponse({
-    status: 200,
-    description: 'Specific user fetched successfully',
-    type: UserResponseDTO,
-  })
-  async getUserById(@Param() id: string): Promise<Partial<UserResponseDTO>> {
-    return {} as Partial<UserResponseDTO>;
-  }
 
   @Post()
   @ApiOperation({
@@ -42,12 +16,25 @@ export class UserController {
   @ApiResponse({
     status: 201,
     description: 'A new user was created successfully',
-    type: UserResponseDTO,
+    type: GetUserResponseDTO,
   })
   async createUser(
     @Body() user: CreateUserRequestDTO,
-  ): Promise<UserResponseDTO> {
+  ): Promise<GetUserResponseDTO> {
     const createdUser = await this.createUserUseCase.execute(user);
     return createdUser;
+  }
+
+  @Get('all')
+  @ApiOperation({
+    summary: 'List all users',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Users list fetched successfully',
+    type: [GetUserResponseDTO],
+  })
+  async getUsers(): Promise<GetUserResponseDTO[]> {
+    return {} as GetUserResponseDTO[];
   }
 }
