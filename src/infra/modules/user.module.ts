cf5bed3fc58d694aaab6fdfa065cc958d8d21133
Julio Brazao <julio@brazao.dev>
2025-03-31 20:26:53 -0400

    Removing temporarily DB providers

--- src/infra/modules/user.module.ts
+++ src/infra/modules/user.module.ts
@@ -1,22 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from '@/presentation/controllers/user.controller';
 import { CreateUserUseCase } from '@/domain/usecases/create-user.usecase';
-import { UserMongoRepository } from '@/infra/repositories/user-mongo.repository';
-import { MongooseModule } from '@nestjs/mongoose';
-import {
-  UserMongoSchema,
-  UserMongoSchemaFactory,
-} from '../repositories/user-mongo.schema';
-import { MysqlModule } from '@/infra/providers/mysql.module';
+import { UserService } from '../providers/services/user.service';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: UserMongoSchema.name, schema: UserMongoSchemaFactory },
-    ]),
-    MysqlModule,
-  ],
   controllers: [UserController],
-  providers: [CreateUserUseCase, UserMongoRepository],
+  providers: [CreateUserUseCase, UserService],
 })
 export class UserModule {}
