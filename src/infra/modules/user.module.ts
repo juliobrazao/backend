52b122d66d274d295caefc22de595a395d63228c
Julio Brazao <julio@brazao.dev>
2025-03-31 20:37:08 -0400

    Importing all use cases

--- src/infra/modules/user.module.ts
+++ src/infra/modules/user.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { UserController } from '@/presentation/controllers/user.controller';
 import { CreateUserUseCase } from '@/domain/usecases/create-user.usecase';
 import { UserService } from '../providers/services/user.service';
+import { GetAllUsersUseCase } from '@/domain/usecases/get-all-users.usecase';
+import { GetUserByIdUseCase } from '@/domain/usecases/get-user-by-id.usecase';
 
 @Module({
   controllers: [UserController],
-  providers: [CreateUserUseCase, UserService],
+  providers: [
+    CreateUserUseCase,
+    GetAllUsersUseCase,
+    GetUserByIdUseCase,
+    UserService,
+  ],
 })
 export class UserModule {}
