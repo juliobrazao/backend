2a3aab6437e626061648783872ac96e5cab82927
Julio Brazao <julio@brazao.dev>
2025-04-06 00:14:14 -0400

    Removing all keycloak assets from BE

--- src/infra/modules/app.module.ts
+++ src/infra/modules/app.module.ts
@@ -2,18 +2,8 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MySQLModule } from './mysql.module';
 import { UserModule } from './user.module';
-import { KeycloakModule } from '../auth/keycloak.module';
-import { AuthModule } from './auth.module';
-import { ProtectedModule } from './protected.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    MySQLModule,
-    UserModule,
-    KeycloakModule,
-    AuthModule,
-    ProtectedModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), MySQLModule, UserModule],
 })
 export class AppModule {}
--- src/infra/modules/protected.module.ts
+++
@@ -1,7 +0,0 @@
-import { ProtectedController } from '@/presentation/controllers/protected.controller';
-import { Module } from '@nestjs/common';
-
-@Module({
-  controllers: [ProtectedController],
-})
-export class ProtectedModule {}
