23a75455bf5656eadf36262ed7e034c7b740a16e
Julio Brazao <julio@brazao.dev>
2025-08-05 21:47:25 -0400

    Removing temporarily unused imports from main app module

--- src/infra/modules/app.module.ts
+++ src/infra/modules/app.module.ts
@@ -1,15 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MySQLModule } from './mysql.module';
-import { UserModule } from './user.module';
 import { MongoModule } from './mongo.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    // MySQLModule,
-    MongoModule,
-    // UserModule,
-  ],
+  imports: [MongoModule],
 })
 export class AppModule {}
