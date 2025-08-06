276464087a365a4b108b1ebf065192fed73f4436
Julio Brazao <julio@brazao.dev>
2025-08-05 21:38:12 -0400

    Mongo config with mongoose instead of typeorm

--- src/infra/modules/mongo.module.ts
+++ src/infra/modules/mongo.module.ts
@@ -1,35 +1,16 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UserMongo } from '../repositories/mongo/entities/user-mongo.entity';
-import IMongoProxyRepository from '@/domain/repositories/abstract-mongo-proxy.repository';
-import MongoRepositoryProxy from '../providers/mongo-repository-proxy.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
+    MongooseModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'mongodb',
-        url: `mongodb://${configService.get<string>('MONGO_USER')}:${configService.get<string>('MONGO_PASSWORD')}@${configService.get<string>('MONGO_HOST')}:${configService.get<string>('MONGO_PORT')}/${configService.get<string>('MONGO_DATABASE')}?authSource=admin`,
-        synchronize: true,
-        entities: [
-          __dirname + '/../repositories/mongo/entities/*.entity{.ts,.js}',
-        ],
+        uri: `mongodb://${configService.get<string>('MONGO_USER')}:${configService.get<string>('MONGO_PASSWORD')}@${configService.get<string>('MONGO_HOST')}:${configService.get<string>('MONGO_PORT')}/${configService.get<string>('MONGO_DATABASE')}?authSource=admin`,
       }),
     }),
-    TypeOrmModule.forFeature([UserMongo]),
   ],
-  providers: [
-    {
-      provide: IMongoProxyRepository,
-      useClass: MongoRepositoryProxy,
-    },
-  ],
-  exports: [IMongoProxyRepository],
 })
 export class MongoModule {}
